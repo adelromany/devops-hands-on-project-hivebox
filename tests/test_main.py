8c1c8d119686475e25cc92415d1d9f51e824a2d5
adelromany <adelromany99@gmail.com>
2026-07-21 00:32:57 +0300

    Fix flake8 lint errors

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,8 +3,9 @@ from src.main import app
 
 client = TestClient(app)
 
+
 def test_root():
     response = client.get("/")
-    
+
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome to HiveBox!"}
